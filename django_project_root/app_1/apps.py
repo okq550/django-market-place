a40fd2ab848f802e7e498d6e13132b2acadb4531
Osama Khd <okq550@gmail.com>
2025-10-12 15:43:58 +0300

    Add sample route in app_1

--- django_project_root/app_1/apps.py
+++ django_project_root/app_1/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class App1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_1'
+    name = 'django_project_root.app_1'
+
